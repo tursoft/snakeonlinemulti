b70d683b9ca5b24b8425c870e0685b1beb55c923
Muhammet Turşak <muhammet.tursak@gamyte.com>
2020-05-12 23:15:50 +0300

    heroku: fix deploy scripts

--- .heroku/run.sh
+++ .heroku/run.sh
@@ -2,37 +2,10 @@
 echo "Client: Check OS version =============="
 cat /etc/os-release
 
-# Install Nodejs
-echo "Client: Install Nodejs =============="
-# curl -sL https://deb.nodesource.com/setup_12.x |
-# apt-get update
-# apt install -y nodejs
-
-echo "Client: Node and Npm Versions =============="
-node --version
-npm --version
-
-
-# Prepare Client
-echo "Client: Install Dependencies =============="
-cd ./client
-npm install
-
-# Build Angular
-echo "Client: Build Angular =============="
-npm run build
-
-# Copy Client Layer to Server Layer to Host
-echo "Client: Copy to Dist to Server Layer =============="
-mv ./client/dist ./server/src/views -y
-
-# Helm Chart - Copy to Server Layer
-echo "Helm Chart - Copy to Server Layer =============="
-mv ./charts/*.* ./server/src/views/charts -y
+echo "Client: Copy to Server =============="
+mv ./.heroku ./server/.heroku-scripts
+mv ./client ./server/client
 
 # Debug - List Folder Content
 echo "Debug - List Folder Content =============="
-cd ../server/src/views
-ls -la ./
-ls -la ./charts
-cat ./charts/index.yaml
+ls -la ./server
