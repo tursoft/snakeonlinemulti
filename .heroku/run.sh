18570a12447ba07efdeec38d155ae97c4a04504e
Muhammet Turşak <muhammet.tursak@gamyte.com>
2020-05-12 22:51:34 +0300

    heroku - custom deployment script to host client in server node app

---
+++ .heroku/run.sh
@@ -0,0 +1,23 @@
+# Prepare Client
+echo "Client: Install Dependencies =============="
+cd ./client
+npm i
+
+# Build Angular
+echo "Client: Build Angular =============="
+npm run build
+
+# Copy Client Layer to Server Layer to Host
+echo "Client: Copy to Dist to Server Layer =============="
+mv ./client/dist ./server/src/views -y
+
+# Helm Chart - Copy to Server Layer
+echo "Helm Chart - Copy to Server Layer =============="
+mv ./charts/*.* ./server/src/views/charts -y
+
+# Debug - List Folder Content
+echo "Debug - List Folder Content =============="
+cd ../server/src/views
+ls -la ./
+ls -la ./charts
+cat ./charts/index.yaml
